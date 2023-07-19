5ea08a1b84a161b8c0680c1f4a1664f708c301c8
Wonchul-Jung88 <36287869+Wonchul-Jung88@users.noreply.github.com>
2023-07-19 17:04:55 +0900

    Add bouncing web attack

--- Indie Game Challenge/Assets/_Scripts/Common/Combat/SpiderWebAttack.cs
+++ Indie Game Challenge/Assets/_Scripts/Common/Combat/SpiderWebAttack.cs
@@ -6,31 +6,23 @@ public class SpiderWebAttack : MonoBehaviour
     public float shootForce = 10f;
     public float webLifeTime = 5f; // Web will be destroyed after this many seconds
     public Transform webShooter; // Assign this in the inspector
-    public float cooldownTime = 2f; // Time in seconds between web shots
+    public float angle = 0f; // New public property for the angle
 
-    private float lastShootTime;
-
-    public void ShootWeb() // Make this function public so it can be called from other scripts
+    public void ShootWeb()
     {
-        // Only shoot a web if enough time has passed since the last web shot
-        if (Time.time - lastShootTime >= cooldownTime)
+        // Create a new web at the webShooter's position.
+        GameObject web = Instantiate(webPrefab, webShooter.position, Quaternion.identity);
+
+        // Assume the web has a Rigidbody component and apply force to it.
+        Rigidbody rb = web.GetComponent<Rigidbody>();
+        if (rb != null)
         {
-            // Create a new web at the webShooter's position.
-            GameObject web = Instantiate(webPrefab, webShooter.position, Quaternion.identity);
-
-            // Assume the web has a Rigidbody component and apply force to it.
-            Rigidbody rb = web.GetComponent<Rigidbody>();
-            if (rb != null)
-            {
-                Vector3 shootDirection = webShooter.forward;
-                rb.AddForce(shootDirection * shootForce, ForceMode.Impulse);
-            }
-
-            // Destroy the web after a certain amount of time
-            Destroy(web, webLifeTime);
-
-            // Update the last shoot time
-            lastShootTime = Time.time;
+            // Calculate the shoot direction based on the provided angle
+            Vector3 shootDirection = Quaternion.Euler(0, 0, angle) * webShooter.forward;
+            rb.AddForce(shootDirection * shootForce, ForceMode.Impulse);
         }
+
+        // Destroy the web after a certain amount of time
+        Destroy(web, webLifeTime);
     }
 }
